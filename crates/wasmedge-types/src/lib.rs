ac0613f19c0bd16fc3f4a75be18379a2161e6583
Xin Liu <sam@secondstate.io>
2023-06-14 15:43:18 +0800

    [CI] Update release workflows (#5)
    * ci(release-macro): update workflow
    
    Signed-off-by: Xin Liu <sam@secondstate.io>
    
    * ci(release-sys): update workflow
    
    Signed-off-by: Xin Liu <sam@secondstate.io>
    
    * ci(release-types): update workflow
    
    Signed-off-by: Xin Liu <sam@secondstate.io>
    
    * ci(release-sdk): update workflow
    
    Signed-off-by: Xin Liu <sam@secondstate.io>
    
    * chore(async-wasi): update cargo file
    
    Signed-off-by: Xin Liu <sam@secondstate.io>
    
    * chore(rust-types): update cargo file
    
    Signed-off-by: Xin Liu <sam@secondstate.io>
    
    * chore(rust-macro): update cargo file
    
    Signed-off-by: Xin Liu <sam@secondstate.io>
    
    * chore(rust-sys): update cargo file
    
    Signed-off-by: Xin Liu <sam@secondstate.io>
    
    * ci(release-async-wasi): publish API doc
    
    Signed-off-by: Xin Liu <sam@secondstate.io>
    
    * ci(release-async-wasi): add working directory
    
    Signed-off-by: Xin Liu <sam@secondstate.io>
    
    ---------
    
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- crates/wasmedge-types/src/lib.rs
+++ crates/wasmedge-types/src/lib.rs
@@ -1,3 +1,8 @@
+#![doc(
+    html_logo_url = "https://github.com/cncf/artwork/blob/master/projects/wasm-edge-runtime/icon/color/wasm-edge-runtime-icon-color.png?raw=true",
+    html_favicon_url = "https://raw.githubusercontent.com/cncf/artwork/49169bdbc88a7ce3c4a722c641cc2d548bd5c340/projects/wasm-edge-runtime/icon/color/wasm-edge-runtime-icon-color.svg"
+)]
+
 //! The [wasmedge-types](https://crates.io/crates/wasmedge-types) crate defines a group of common data structures used by both [wasmedge-sdk](https://crates.io/crates/wasmedge-sdk) and [wasmedge-sys](https://crates.io/crates/wasmedge-sys) crates.
 //!
 //! See also
