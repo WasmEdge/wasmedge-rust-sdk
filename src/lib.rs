8192f187b697e1313f7ed9a37a354dde02b5c0a6
Xin Liu <sam@secondstate.io>
2023-06-28 14:09:27 +0000

    chore(rust-sdk): update rustdoc
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- src/lib.rs
+++ src/lib.rs
@@ -7,55 +7,55 @@
 #![allow(clippy::vec_init_then_push)]
 
 //! # Overview
-//! 
+//!
 //! WasmEdge Rust SDK provides idiomatic [Rust](https://www.rust-lang.org/) language bindings for [WasmEdge](https://wasmedge.org/)
-//! 
+//!
 //! **Notice:** This project is still under active development and not guaranteed to have a stable API.
-//! 
+//!
 //! - [Documentation](https://wasmedge.org/docs/)
 //! - [WasmEdge website](https://wasmedge.org/)
 //! - [WasmEdge GitHub Page](https://github.com/WasmEdge/WasmEdge)
 //! - [WasmEdge Rust SDK GitHub Page](https://github.com/WasmEdge/wasmedge-rust-sdk)
 //! - [WasmEdge Rust SDK Examples](https://github.com/second-state/wasmedge-rustsdk-examples)
-//! 
+//!
 //! ## Get Started
-//! 
+//!
 //! Since this crate depends on the WasmEdge C API, it needs to be installed in your system first. Please refer to [WasmEdge Installation and Uninstallation](https://wasmedge.org/book/en/quick_start/install.html) to install the WasmEdge library. The versioning table below shows the version of the WasmEdge library required by each version of the `wasmedge-sdk` crate.
-//! 
-//!   | wasmedge-sdk  | WasmEdge lib  | wasmedge-sys  | wasmedge-types| wasmedge-macro| async-wasi|
-//!   | :-----------: | :-----------: | :-----------: | :-----------: | :-----------: | :-------: |
-//!   | 0.9.0         | 0.13.0        | 0.14.0        | 0.4.2         | 0.4.0         | 0.0.1     |
-//!   | 0.8.1         | 0.12.1        | 0.13.1        | 0.4.1         | 0.3.0         | -         |
-//!   | 0.8.0         | 0.12.0        | 0.13.0        | 0.4.1         | 0.3.0         | -         |
-//!   | 0.7.1         | 0.11.2        | 0.12.2        | 0.3.1         | 0.3.0         | -         |
-//!   | 0.7.0         | 0.11.2        | 0.12          | 0.3.1         | 0.3.0         | -         |
-//!   | 0.6.0         | 0.11.2        | 0.11          | 0.3.0         | 0.2.0         | -         |
-//!   | 0.5.0         | 0.11.1        | 0.10          | 0.3.0         | 0.1.0         | -         |
-//!   | 0.4.0         | 0.11.0        | 0.9           | 0.2.1         | -             | -         |
-//!   | 0.3.0         | 0.10.1        | 0.8           | 0.2           | -             | -         |
-//!   | 0.1.0         | 0.10.0        | 0.7           | 0.1           | -             | -         |
-//! 
+//!
+//! | wasmedge-sdk  | WasmEdge lib  | wasmedge-sys  | wasmedge-types| wasmedge-macro| async-wasi|
+//! | :-----------: | :-----------: | :-----------: | :-----------: | :-----------: | :-------: |
+//! | 0.9.0         | 0.13.0        | 0.14.0        | 0.4.2         | 0.4.0         | 0.0.1     |
+//! | 0.8.1         | 0.12.1        | 0.13.1        | 0.4.1         | 0.3.0         | -         |
+//! | 0.8.0         | 0.12.0        | 0.13.0        | 0.4.1         | 0.3.0         | -         |
+//! | 0.7.1         | 0.11.2        | 0.12.2        | 0.3.1         | 0.3.0         | -         |
+//! | 0.7.0         | 0.11.2        | 0.12          | 0.3.1         | 0.3.0         | -         |
+//! | 0.6.0         | 0.11.2        | 0.11          | 0.3.0         | 0.2.0         | -         |
+//! | 0.5.0         | 0.11.1        | 0.10          | 0.3.0         | 0.1.0         | -         |
+//! | 0.4.0         | 0.11.0        | 0.9           | 0.2.1         | -             | -         |
+//! | 0.3.0         | 0.10.1        | 0.8           | 0.2           | -             | -         |
+//! | 0.1.0         | 0.10.0        | 0.7           | 0.1           | -             | -         |
+//!
 //! WasmEdge Rust SDK can automatically search the following paths for the WasmEdge library:
-//! 
+//!
 //! - `$HOME/.wasmedge` (Linux/macOS)
 //! - `/usr/local` (Linux/macOS)
-//! 
+//!
 //! If you have installed the WasmEdge library in a different path, you can set the `WASMEDGE_INCLUDE_DIR` and `WASMEDGE_LIB_DIR` environment variables to the path of the WasmEdge library.
-//! 
+//!
 //! **Notice:** The minimum supported Rust version is 1.68.
-//! 
+//!
 //! ## Examples
-//! 
+//!
 //! The [Examples of WasmEdge RustSDK](https://github.com/second-state/wasmedge-rustsdk-examples) repo contains a number of examples that demonstrate how to use the WasmEdge Rust SDK.
-//! 
+//!
 //! ## Contributing
-//! 
+//!
 //! Please read the [contribution guidelines](https://github.com/WasmEdge/wasmedge-rust-sdk/blob/main/CONTRIBUTING.md) on how to contribute code.
-//! 
+//!
 //! ## License
-//! 
+//!
 //! This project is licensed under the terms of the [Apache 2.0 license](https://github.com/tensorflow/rust/blob/HEAD/LICENSE).
-//! 
+//!
 
 #[doc(hidden)]
 pub mod caller;
@@ -121,22 +121,28 @@ pub use wasmedge_macro::{async_host_function, host_function};
 /// WebAssembly value type.
 pub type WasmValue = wasmedge_sys::types::WasmValue;
 
+/// This is a workaround solution to the [`never`](https://doc.rust-lang.org/std/primitive.never.html) type in Rust. It will be replaced by `!` once it is stable.
 pub type NeverType = wasmedge_types::NeverType;
 
+#[doc(hidden)]
 pub type CallingFrame = wasmedge_sys::CallingFrame;
 
+/// Defines the signature of a host function.
 pub type HostFn<T> = wasmedge_sys::HostFn<T>;
 
+/// Defines the signature of a finalizer funtion that is used to free the host data.
 pub type Finalizer = wasmedge_sys::plugin::Finalizer;
 
+/// Defines the types used in the `async` scenarios.
 #[cfg(all(feature = "async", target_os = "linux"))]
 pub mod r#async {
+    /// The state of an asynchronous task.
     pub type AsyncState = wasmedge_sys::r#async::AsyncState;
+    /// Defines the signature of an asynchronous host function.
     pub type AsyncHostFn<T> = wasmedge_sys::AsyncHostFn<T>;
+    /// Type of wasi context that is used to configure the wasi environment.
+    pub type WasiCtx = wasmedge_sys::WasiCtx;
 }
-
-#[cfg(all(feature = "async", target_os = "linux"))]
-pub type WasiCtx = wasmedge_sys::WasiCtx;
 
 /// The object that is used to perform a [host function](crate::Func) is required to implement this trait.
 pub trait Engine {
