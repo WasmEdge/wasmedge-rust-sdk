3b6fbfcea847e6cbb7d0e0df75c10880fbc155db
Xin Liu <sam@secondstate.io>
2023-07-26 06:47:03 +0000

    feat!(rust-sdk): add `async` mod
    Signed-off-by: Xin Liu <sam@secondstate.io>

---
+++ src/async.rs
@@ -0,0 +1,46 @@
+/// The state of an asynchronous task.
+pub type AsyncState = wasmedge_sys::r#async::fiber::AsyncState;
+
+/// Represents a wasi module instance.
+#[derive(Debug, Clone)]
+pub(crate) struct WasiInstance(pub(crate) wasmedge_sys::r#async::AsyncWasiModule);
+
+#[derive(Debug)]
+pub struct WasiContext {
+    pub(crate) inner: async_wasi::WasiCtx,
+}
+impl WasiContext {
+    pub fn new(
+        args: Option<Vec<&str>>,
+        envs: Option<Vec<(&str, &str)>>,
+        preopens: Option<Vec<(&str, &str)>>,
+    ) -> Self {
+        let mut inner = async_wasi::WasiCtx::new();
+
+        if let Some(args) = args {
+            inner.push_args(args.iter().map(|x| x.to_string()).collect());
+        }
+        if let Some(envs) = envs {
+            inner.push_envs(envs.iter().map(|(k, v)| format!("{}={}", k, v)).collect());
+        }
+        if let Some(preopens) = preopens {
+            for (host_dir, guest_dir) in preopens {
+                inner.push_preopen(
+                    std::path::PathBuf::from(host_dir),
+                    std::path::PathBuf::from(guest_dir),
+                )
+            }
+        }
+
+        Self { inner }
+    }
+
+    pub fn exit_code(&self) -> u32 {
+        self.inner.exit_code
+    }
+}
+impl Default for WasiContext {
+    fn default() -> Self {
+        Self::new(None, None, None)
+    }
+}
